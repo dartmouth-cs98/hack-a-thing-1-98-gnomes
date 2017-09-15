6084ef148f5e46219f33d914607aa3c2abe28e48
Ross Bower <ross@ross-mbp.wireless.dartmouth.edu>
2017-09-15 11:35:06 -0400

    template

--- switch.js
+++ switch.js
@@ -2,3 +2,6 @@ for (var i = 0; document.images.length; i++) {
   // Change the url to anyting you want!
   document.images[i].src = "YOUR_IMG_URL"
 };
+
+
+// Write a function that randomly selects words to replace with a blank and its part of speech
